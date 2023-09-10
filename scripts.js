326eb509b05515cae4613dad20da56527316281e
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2023-09-10 13:52:56 +1000

    Added button to remove book

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 const books = [];
+const add = document.querySelector(".add");
 
 function Book() {
 
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+add.addEventListener("click", event => {
+    console.log("POP FOR BOOK INFO");
+});
